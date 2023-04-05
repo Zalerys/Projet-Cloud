d3f327ded29055b3363a1e49dcd08bf5ffaebbe0
Alexandre Grodent <59176226+alexandregdnt@users.noreply.github.com>
2023-04-05 16:35:04 +0200

    :construction: Front-end work in progress

---
+++ front/src/App.tsx
@@ -0,0 +1,6 @@
+import './index.css';
+
+function App() {
+  return <div className=""></div>;
+}
+export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import reportWebVitals from './utils/reportWebVitals';
-
-import store from './redux/store/store';
-import router from './router';
-import './assets/styles/global.scss';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </Provider>,
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
