d113eeece63a84ec802b6238d93d045e6e4e3db5
constans <gustave.constans@fairlymade.com>
2023-04-06 22:40:56 +0200

    feature/serveurcontent-component

--- front/src/containers/ServeurContent.tsx
+++ front/src/containers/ServeurContent.tsx
@@ -1,6 +1,10 @@
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getFetch } from '../controller/getFetch';
 import Button from '../components/ButtonWhite/ButtonWhite';
 import CardDBUser from '../components/CardDBUser/CardDBUser';
+import CardStorage from '../components/CardStorage';
+import CardListBackup from '../components/CardListBackup';
 import Title from '../components/Title';
 export default function ServeurContent() {
   const navigate = useNavigate();
@@ -13,9 +17,15 @@ export default function ServeurContent() {
   const toHomePage = (event: React.MouseEvent<HTMLButtonElement>) => {
     navigate('/homepage');
   };
+
+  // async function getData() {
+  //   var user = sessionStorage.getItem('user');
+  //   const data = await getFetch(`/users/serveur/db/${user}`);
+  //   setState({ ...state, user_id: data.id });
+  // }
+
   return (
     <div className="flex-col">
-      <Title name="Serveur" />
       <div className="flex justify-between">
         <div className="m-4">
           <Button onClick={toHomePage} name="Retour" />
@@ -24,8 +34,17 @@ export default function ServeurContent() {
           <Button onClick={toLogout} name="Déconnexion" />
         </div>
       </div>
-      <div className="flex justify-end">
-        <CardDBUser />
+      <div className="text-center">
+        <Title name="Serveur" />
+      </div>
+      <div className="flex  gap-4">
+        <div className="flex-1 w-90">
+          <CardListBackup />
+        </div>
+        <div>
+          <CardDBUser name="testname" password="testpassword" />
+          <CardStorage />
+        </div>
       </div>
     </div>
   );
