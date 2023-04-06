b89b03fa5852c820d3e095a19bb2f2a6ce5bd631
cazenave <cyprien@maplab.green>
2023-04-06 20:28:32 +0200

    add ServerController

---
+++ server/src/Controllers/ServerController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Exceptions\ServerException;
+use App\Factories\PDOFactory;
+use App\Framework\Entity\BaseController;
+use App\Framework\Route\Route;
+use App\Helpers\Tools;
+use App\Managers\ServerManager;
+use App\Types\HttpMethods;
+use Exception;
+
+class ServerController extends BaseController
+{
+    #[Route("/servers/{id}", name: "server", methods: [HttpMethods::GET])]
+    public function serverView(int $id): void
+    {
+        $server = null;
+        try {
+            $server = (new ServerManager(new PDOFactory()))->findOne($id);
+
+            http_response_code(200);
+            $this->renderJSON([
+                "server" => $server->toArray(),
+            ]);
+        }
+        catch (Exception $e) {
+            http_response_code(404);
+            $this->renderJSON([
+                "message" => "user not found"
+            ]);
+        }
+    }
+    #[Route("/server/{id}", name: "server-list", methods: [HttpMethods::GET])]
+    public function serverProjectsList(int $id): void
+    {
+        $server = null;
+        try {
+            $server = (new ServerManager(new PDOFactory()))->findOne($id);
+            http_response_code(200);
+            $this->renderJSON([
+                "projects" => $server->getProjects(),
+            ]);
+        } catch (Exception $e) {
+            http_response_code(404);
+            $this->renderJSON([
+                "message" => "server not found"
+            ]);
+        }
+    }
+
+
+}
