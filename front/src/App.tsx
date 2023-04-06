841d66ca14ba4c09e24b6fa34dbf407b12d38c1f
M3dzZ <mehdichaibi05@gmail.com>
2023-04-06 10:24:36 +0200

    create routes in App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,21 @@
 import './index.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './layouts/Register';
+import Login from './layouts/Login';
+import Home from './layouts/Homepage';
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 export default App;
