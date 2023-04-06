57edad9948f1a34ec6d0564cb4b88808f732385c
constans <gustave.constans@fairlymade.com>
2023-04-06 14:55:25 +0200

    update/add-route-profil/serveur

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './layouts/Register';
 import Login from './layouts/Login';
 import Home from './layouts/Homepage';
+import Profil from './layouts/Profil';
+import Serveur from './layouts/Serveur';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/serveur" element={<Serveur />} />
+          <Route path="/profil" element={<Profil />} />
         </Routes>
       </BrowserRouter>
     </div>
