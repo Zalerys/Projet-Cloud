fa5efcb965b389431ad8af3504fc2e0762c3bfdf
M3dzZ <mehdichaibi05@gmail.com>
2023-04-06 16:31:42 +0200

    create register content

---
+++ front/src/containers/AuthenticationContent.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Title from '../components/Title';
+import { postFetch } from '../controller/postFetch';
+
+export default function AuthenticationContent() {
+  const navigate = useNavigate();
+  const [err, setErr] = useState<string | null>('');
+
+  const [state, setState] = useState({
+    name: '',
+    password: '',
+    key_ssh: '',
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    key: keyof typeof state,
+  ) => {
+    setState({ ...state, [key]: event.target.value });
+  };
+
+  async function register() {
+    if ((await postFetch('/authentication', state)) === false) {
+      setErr('Account already created or invalid');
+    } else {
+      sessionStorage.setItem('', state.password);
+      navigate('/homepage');
+    }
+  }
+
+  return (
+    <div className="relative m-auto text-center ">
+      <Title name={'Create your account!'} />
+
+      <form className="flex flex-col items-center gap-7" action="post">
+        <Input placeholder="Name" required={true}  key="name" onChange={(event) => handleChange(event, "name")}/>
+        <Input placeholder="Password"  required={true}  key="password" onChange={(event) => handleChange(event, "password")}/>
+        <Input placeholder="Key ssh" onChange={(event) => handleChange(event, "key_ssh")}/>
+        <Button name="Register" onClick={register}/>
+      </form>
+      <span className="absolute mb-3 text-sm italic left-5 bottom-24 text-violet">
+        Optional
+      </span>
+    </div>
+  );
+}
