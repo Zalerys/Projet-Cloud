5abf79f642ee26b9b97eb9f6fc298f6525e59963
juroche42 <94986757+juroche42@users.noreply.github.com>
2023-04-07 00:59:44 +0200

    Ajout des scripts .sh restant

---
+++ server/src/scripts/addserver.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Récupération des paramètres
+
+servername="$1"
+username="$2"
+
+#Creation Repertoire du Serveur
+sudo  mkdir /var/www/html/$username/$servername
+
+#Création du fichier config
+
+template="
+server {
+        listen 80;
+        listen [::]:80;
+
+        server_name www.$servername.fr;
+
+        root /var/www/html/$username/$servername;
+        index index.html index.php
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+        error_page 404 /404.htm;
+        error_page 403 /403.htm;
+        error_page 500 /500.htm;
+        access_log /var/log/nginx/$servername.log;
+        error_log /var/log/nginx/$servername-error.log;
+                location ~* ^.+\.(xml|ogg|ogv|svg|svgz|eot|otf|woff)$ {
+                return 404;
+        }
+}
+
---
+++ server/src/scripts/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+filename='$1'
+
+tar –cvzf $filename
