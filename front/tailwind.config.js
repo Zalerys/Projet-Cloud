b440a6b0e3e77f29ff6dcf8dc0b7c38373553eae
M3dzZ <mehdichaibi05@gmail.com>
2023-04-06 10:23:44 +0200

    add default color in tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      violet: '#867BFF',
+      grey: '#F2F2F2',
+      whiteViolet: '#F7F7FF',
+    },
   },
   plugins: [],
 };
