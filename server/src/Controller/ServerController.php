d8d31f9a930c0e874917765ecad3e71d3e8ffde9
Alexandre Grodent <59176226+alexandregdnt@users.noreply.github.com>
2023-04-26 14:28:28 +0200

    :construction::package: [ServerController] server details + add Serializer

--- server/src/Controller/ServerController.php
+++ server/src/Controller/ServerController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\ServerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ServerController extends AbstractController
 {
-    #[Route('/server', name: 'app_server')]
+    #[Route('/server', name: 'app_server', methods: ['POST'])]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ServerController.php',
         ]);
+    }
+
+    #[Route("/server/{id}", name: "server-details", methods: ['GET'])]
+    public function serverView(int $id, SerializerInterface $serializer, ServerRepository $serverRepository): JsonResponse
+    {
+        $server = $serverRepository->find($id);
+        if ($server) {
+            $jsonServer = $serializer->serialize($server, 'json');
+            return new JsonResponse($jsonServer, Response::HTTP_OK, [], true);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/server/{token}', name: "create_server", methods: ['POST'])]
+    public function createServer(string $token) {
+
+    }
+
+    #[Route('/server/{id}', name: "delete_server", methods: ['DELETE'])]
+    public function deleteServer(int $id) {
+
     }
 }
