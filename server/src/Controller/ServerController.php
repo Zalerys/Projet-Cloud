93bf301ce82ad6bdc5c09919bbab2f47678b8694
juroche42 <94986757+juroche42@users.noreply.github.com>
2023-04-27 15:01:59 +0200

    Insert post Server
    AJout de la root insert Server dans le controller

--- server/src/Controller/ServerController.php
+++ server/src/Controller/ServerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Server;
 use App\Repository\ServerRepository;
+//use http\Env\Request;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -20,6 +24,14 @@ class ServerController extends AbstractController
         ]);
     }
 
+    #[Route('/server', name: 'server_list', methods: ['GET'])]
+    public function serverList(SerializerInterface $serializer, ServerRepository $serverRepository): JsonResponse
+    {
+        $servers = $serverRepository->findAll();
+        $jsonServers = $serializer->serialize($servers, 'json');
+        return new JsonResponse($jsonServers, Response::HTTP_OK, [], true);
+    }
+
     #[Route("/server/{id}", name: "server-details", methods: ['GET'])]
     public function serverView(int $id, SerializerInterface $serializer, ServerRepository $serverRepository): JsonResponse
     {
@@ -31,8 +43,29 @@ class ServerController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
-    #[Route('/server/{token}', name: "create_server", methods: ['POST'])]
-    public function createServer(string $token) {
+    #[Route('/server/{id}', name: "create_server", methods: ['POST'])]
+    public function createServer(int $id, Request $request, ServerRepository $serverRepository, UserRepository $userRepository): Response
+    {
+        // Récupérer les données du formulaire
+        $data = json_decode($request->getContent(), true);
+
+        // Récupérer l'utilisateur
+        $user = $userRepository->find($id);
+
+        // Créer une nouvelle instance de l'entité User
+        $server = (new Server())
+            ->setName($data['name'])
+            ->setStorageSize($data['storage_size'])
+            ->setBackupsFolderPath($data['backups_folder_path'])
+            ->setAutoBackupsTime(new \DateTimeImmutable())
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->addUser($user);
+
+        // Persister l'entité dans la base de données
+        $serverRepository->save($server, true);
+
+        // Retourner une réponse
+        return new Response('Server created', Response::HTTP_CREATED);
 
     }
 
