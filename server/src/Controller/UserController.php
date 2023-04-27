a81e646eae1f52918e57c9a0469774071bbb1c73
juroche42 <94986757+juroche42@users.noreply.github.com>
2023-04-27 19:30:11 +0200

    Modify Pwd
    ajout méthode modify pwd

--- server/src/Controller/UserController.php
+++ server/src/Controller/UserController.php
@@ -63,6 +63,29 @@ class UserController extends AbstractController
         return $this->redirectToRoute('user_list');
     }
 
+    #[Route('/api/users/pwd/{id}', name: "update_pwd_user", methods: ['PUT'])]
+    public function modifyPassword(int $id, Request $request, UserRepository $entityRepository, User $user): Response {
+        //Verifier si l'utilisateur existe
+        $user = $entityRepository->find($id);
+        if (!$user) {
+            return $this->json([
+                'error' => 'L\'utilisateur n\'existe pas'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        // Récupérer les données du formulaire
+        $data = json_decode($request->getContent(), true);
+
+        // Créer une nouvelle instance de l'entité User
+        $user->setPassword($data['password']);
+
+        // Persister l'entité dans la base de données
+        $entityRepository->save($user, true);
+
+        // Retourner une réponse
+        return $this->redirectToRoute('user_details', ['id' => $user->getId()]);
+    }
+
     #[Route('/api/users/{id}', name: "delete_user", methods: ['DELETE'])]
     public function deleteUser(int $id, Request $request, UserRepository $entityRepository, User $user): Response {
         //Verifier si l'utilisateur existe
